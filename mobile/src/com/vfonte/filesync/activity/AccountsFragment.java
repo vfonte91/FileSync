8c85aad3039f9cf0b460391b496f4ce9172846a6
Vince Fonte <v.fonte91@gmail.com>
2014-02-16 20:09:33 -0500

    Added array adpater

--- mobile/src/com/vfonte/filesync/activity/AccountsFragment.java
+++ mobile/src/com/vfonte/filesync/activity/AccountsFragment.java
@@ -1,23 +1,60 @@
 package com.vfonte.filesync.activity;
 
+import com.vfonte.filesync.FileSync;
 import com.vfonte.filesync.R;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class AccountsFragment extends Fragment  {
+	private ListView listView;
+	private SimpleArrayAdapter adapter;
+	private String[] accounts;
 
-    public AccountsFragment() {
-        // Empty constructor required for fragment subclasses
-    }
+	public AccountsFragment() {
+		// Empty constructor required for fragment subclasses
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_accounts, container, false);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_list, container, false);
+		listView = (ListView) rootView.findViewById(android.R.id.list);
 
-        return rootView;
-    }
+		accounts = new String[] {"A", "B", "C"};
+
+		adapter = new SimpleArrayAdapter(FileSync.getAppContext(), accounts);
+		return rootView;
+	}
+
+	public class SimpleArrayAdapter extends ArrayAdapter<String> {
+		private final Context context;
+		private final String[] values;
+		private final static int layout = R.layout.accounts_list_item;
+
+		public SimpleArrayAdapter(Context context, String[] values) {
+			super(context, layout, values);
+			this.context = context;
+			this.values = values;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			// inflate the item view
+		    View rowView = inflater.inflate(layout, parent, false);
+
+		    // Set the session name text view to name
+		    TextView textView = (TextView) rowView.findViewById(R.id.account_text_view);
+		    textView.setText(this.values[position]);
+			return rowView;
+		}
+	}
 }
