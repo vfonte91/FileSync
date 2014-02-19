89b8984f9af9cb6b0784ba4962fa30620788dd54
Vince Fonte <v.fonte91@gmail.com>
2014-02-18 19:37:52 -0500

    Added FileListActivity

--- mobile/src/com/vfonte/filesync/view/FileListActivity.java
+++ mobile/src/com/vfonte/filesync/view/FileListActivity.java
@@ -1,7 +1,26 @@
 package com.vfonte.filesync.view;
 
+import com.vfonte.filesync.R;
+import com.vfonte.filesync.R.layout;
+import com.vfonte.filesync.R.menu;
+
+import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
 
 public class FileListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_file_list);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.file_list, menu);
+		return true;
+	}
 
 }
